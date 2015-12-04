4845dddda70eb28f969d642b245a9b226dcb0349
moctargueye <moctar.gueye@isep.fr>
2015-12-04 01:42:37 +0100

    maj-3

--- view/form_connect.php
+++ view/form_connect.php
@@ -7,21 +7,15 @@
     <h1>Connexion</h1>
 
     <p>
-
-    <label for="pseudo">Pseudo :</label><input name="pseudo" type="text" id="pseudo" /><br />
-
-    <label for="password">Mot de Passe :</label><input type="password" name="password" id="password" />
-
+    	<label for="pseudo">Pseudo :</label><input name="pseudo" type="text" id="pseudo" /><br />
+    	<label for="password">Mot de Passe :</label><input type="password" name="password" id="password" />
     </p>
 
     </fieldset>
 
     <p><input type="submit" value="Connexion" /></p></form>
 
-    <a href="register.php">Pas encore inscrit ?</a>
-
-     
-
+    <a href="subscribe.php">Pas encore inscrit ?</a>
     </div>
 
     </body>
