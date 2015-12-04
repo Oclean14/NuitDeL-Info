e1f535fbd8f681075758eda925837d8079a43fc2
rajradjasegar <radjasegar.raj@gmail.com>
2015-12-04 03:40:00 +0100

    index modified

--- view/index.php
+++ view/index.php
@@ -44,22 +44,22 @@
 						<section class="4u 12u(2)">
 							<div class="box highlight">
 								<i class="icon major fa-paper-plane"></i>
-								<h3>This Is Important</h3>
-								<p>Conditions dans lesquelles vivent les populations pauvres vivent</p>
+								<h3>1ère Valeur</h3>
+								<p>Respecter l'humanité</p>
 							</div>
 						</section>
 						<section class="4u 12u(2)">
 							<div class="box highlight">
 								<i class="icon major fa-pencil"></i>
-								<h3>Also Important</h3>
-								<p>Les Conditions à respecter</p>
+								<h3>2ème Valeur</h3>
+								<p>Faire valoir le droit des réfufiés</p>
 							</div>
 						</section>
 						<section class="4u 12u(2)">
 							<div class="box highlight">
 								<i class="icon major fa-wrench"></i>
-								<h3>Probably Important</h3>
-								<p>D'où ils viennent</p>
+								<h3>3ème Valeur</h3>
+								<p>Nous donnons priorité à l'éducation de enfants</p>
 							</div>
 						</section>
 					</div>
@@ -70,8 +70,8 @@
 			<section class="wrapper style2">
 				<div class="container">
 					<header class="major">
-						<h2>A gigantic heading you can use for whatever</h2>
-						<p>With a much smaller subtitle hanging out just below it</p>
+						<h2>Nous vous connectons avec ceux qui ont besoin de vous</h2>
+						<p>Cliquez ici afin de pouvoir discuter avec des réscapés de catastrophes</p>
 					</header>
 				</div>
 			</section>
@@ -84,8 +84,8 @@
 							<div class="box post">
 								<a href="#" class="image left"><img src="images/pic01.jpg" alt="" /></a>
 								<div class="inner">
-									<h3>The First Thing</h3>
-									<p>Duis neque nisi, dapibus sed mattis et quis, nibh. Sed et dapibus nisl amet mattis, sed a rutrum accumsan sed. Suspendisse eu.</p>
+									<h3>Attaque Terroriste à Paris</h3>
+									<p>Paris a été la cible de plusieurs attaques terroristes ?!</p>
 								</div>
 							</div>
 						</section>
@@ -93,8 +93,8 @@
 							<div class="box post">
 								<a href="#" class="image left"><img src="images/pic02.jpg" alt="" /></a>
 								<div class="inner">
-									<h3>The Second Thing</h3>
-									<p>Duis neque nisi, dapibus sed mattis et quis, nibh. Sed et dapibus nisl amet mattis, sed a rutrum accumsan sed. Suspendisse eu.</p>
+									<h3>Réfugiés Syriens</h3>
+									<p>Le Flux migratoire vers l'Europe continue à augmenter</p>
 								</div>
 							</div>
 						</section>
@@ -104,8 +104,8 @@
 							<div class="box post">
 								<a href="#" class="image left"><img src="images/pic03.jpg" alt="" /></a>
 								<div class="inner">
-									<h3>The Third Thing</h3>
-									<p>Duis neque nisi, dapibus sed mattis et quis, nibh. Sed et dapibus nisl amet mattis, sed a rutrum accumsan sed. Suspendisse eu.</p>
+									<h3>Bombardement en Syrie</h3>
+									<p>La population restée en Syrie se retrouve entre les bombardements de la coalition et ceux de l'EI</p>
 								</div>
 							</div>
 						</section>
@@ -113,8 +113,8 @@
 							<div class="box post">
 								<a href="#" class="image left"><img src="images/pic04.jpg" alt="" /></a>
 								<div class="inner">
-									<h3>The Fourth Thing</h3>
-									<p>Duis neque nisi, dapibus sed mattis et quis, nibh. Sed et dapibus nisl amet mattis, sed a rutrum accumsan sed. Suspendisse eu.</p>
+									<h3>Mali</h3>
+									<p>La Poulation Malienne a besoin de votr aide suite aux attentats</p>
 								</div>
 							</div>
 						</section>
@@ -126,8 +126,8 @@
 			<section id="cta" class="wrapper style3">
 				<div class="container">
 					<header>
-						<h2>Are you ready to continue your quest?</h2>
-						<a href="#" class="button">Insert Coin</a>
+						<h2>Do you want to help poor people ?</h2>
+						<a href="#" class="button">Give your money</a>
 					</header>
 				</div>
 			</section>
@@ -137,27 +137,19 @@
 				<div class="container">
 					<div class="row">
 						<section class="3u 6u(2) 12u$(3)">
-							<h3>Links to Stuff</h3>
+							<h3>Liens Utiles</h3>
 							<ul class="links">
-								<li><a href="#">Mattis et quis rutrum</a></li>
-								<li><a href="#">Suspendisse amet varius</a></li>
-								<li><a href="#">Sed et dapibus quis</a></li>
-								<li><a href="#">Rutrum accumsan dolor</a></li>
-								<li><a href="#">Mattis rutrum accumsan</a></li>
-								<li><a href="#">Suspendisse varius nibh</a></li>
-								<li><a href="#">Sed et dapibus mattis</a></li>
+								<li><a href="#">Qui Sommes Nous ?</a></li>
+								<li><a href="#">Quelles sont les zones que nous couvront ?</a></li>
+								<li><a href="#">Nos partenaires</a></li>
 							</ul>
 						</section>
 						<section class="3u 6u$(2) 12u$(3)">
-							<h3>More Links to Stuff</h3>
+							<h3>Aide</h3>
 							<ul class="links">
-								<li><a href="#">Duis neque nisi dapibus</a></li>
-								<li><a href="#">Sed et dapibus quis</a></li>
-								<li><a href="#">Rutrum accumsan sed</a></li>
-								<li><a href="#">Mattis et sed accumsan</a></li>
-								<li><a href="#">Duis neque nisi sed</a></li>
-								<li><a href="#">Sed et dapibus quis</a></li>
-								<li><a href="#">Rutrum amet varius</a></li>
+								<li><a href="#">Assistance</a></li>
+								<li><a href="#">Tous les sujets d'aide</a></li>
+								<li><a href="#">FAQ</a></li>
 							</ul>
 						</section>
 						<section class="6u 12u(2)">
