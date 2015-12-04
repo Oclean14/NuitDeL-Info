d69f926d85aa4268d702f3e5ba2627dc095c395a
rajradjasegar <radjasegar.raj@gmail.com>
2015-12-04 05:14:56 +0100

    raj last commit

---
+++ view/hebergement.php
@@ -0,0 +1,58 @@
+<!DOCTYPE HTML>
+<!--
+	Arcana by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Jeu</title>
+		<?php include("header.php");?> 
+	</head>
+	<body>
+
+	
+			
+		<!-- Main -->
+			<section class="wrapper style1">
+				<div class="container">
+					<div id="content">
+
+						<!-- Content -->
+					
+							<article>
+								<header>
+									<h2>List of user's status</h2>
+									<p>All content, all the time.</p>
+
+
+								</header>
+							
+							</article>
+				
+					</div>
+				</div>
+			</section>			
+
+		<?php include("footer.php");?> 
+
+				<!-- Icons -->
+					<ul class="icons">
+						<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+						<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+						<li><a href="#" class="icon fa-github"><span class="label">GitHub</span></a></li>
+						<li><a href="#" class="icon fa-linkedin"><span class="label">LinkedIn</span></a></li>
+						<li><a href="#" class="icon fa-google-plus"><span class="label">Google+</span></a></li>
+					</ul>
+
+				<!-- Copyright -->
+					<div class="copyright">
+						<ul class="menu">
+							<li>&copy; Untitled. All rights reserved</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+						</ul>
+					</div>
+
+			</div>
+
+	</body>
+</html>
